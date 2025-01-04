d2c0c8c91099e6dbbee468217f4c2e0b47959766
m0t9 <matveykorinenko@gmail.com>
2025-01-04 13:02:55 +0300

    chore: README update, optimize linter

--- pkg/arrcopy/arrcopy.go
+++ pkg/arrcopy/arrcopy.go
@@ -6,32 +6,32 @@ import (
 	"go/types"
 
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 // NewAnalyzer returns analyzer instance for arrcopy linter.
 func NewAnalyzer() *analysis.Analyzer {
 	return &analysis.Analyzer{
-		Name: "arrcopy",
-		Doc:  "reports for-range looping over arrays with copying",
-		Run:  run,
+		Name:     "arrcopy",
+		Doc:      "reports for-range looping over arrays with copying",
+		Run:      run,
+		Requires: []*analysis.Analyzer{inspect.Analyzer},
 	}
 }
 
 func run(pass *analysis.Pass) (any, error) {
-	for _, file := range pass.Files {
-		ast.Inspect(file, func(n ast.Node) bool {
-			rng, ok := n.(*ast.RangeStmt)
-			if !ok {
-				return true
-			}
-			vt := pass.TypesInfo.TypeOf(rng.X)
-			if _, isArray := vt.(*types.Array); isArray {
-				pass.Reportf(rng.Pos(),
-					`for-range loop over array '%s' found. Use for-range over '&%s' instead`,
-					rng.X, rng.X)
-			}
-			return true
-		})
-	}
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	filter := []ast.Node{(*ast.RangeStmt)(nil)}
+
+	inspector.Preorder(filter, func(n ast.Node) {
+		rng := n.(*ast.RangeStmt)
+		vt := pass.TypesInfo.TypeOf(rng.X)
+		if _, isArray := vt.(*types.Array); isArray {
+			pass.Reportf(rng.Pos(),
+				`for-range loop over array '%s' found. Use for-range over '&%s' instead`,
+				rng.X, rng.X)
+		}
+	})
 	return nil, nil
 }
